ecef1b9461b909dc92a6de899dd8736186ea5483
Golovko <golovcozlata@gmail.com>
2017-07-12 00:53:46 +0300

    add !!!

--- 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
         for (int i = 0; i < 5; i++) {
-            System.out.println("Элли самая красивая");
+            System.out.println("Элли самая красивая !!!!!");
         }
     }
 }
